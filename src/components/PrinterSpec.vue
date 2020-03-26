5af442e422de335e060c036e904a44e51988c7be
Josiah Roberts <josiah.david.roberts@gmail.com>
2020-03-27 00:04:08 +0800

    Temporarilly render spec on search results

---
+++ src/components/PrinterSpec.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="spec">
+    <div>
+      <div>
+        <strong>Short Axis:</strong>
+        <span>{{spec.requiredDimensions.shortAxis}} inches</span>
+      </div>
+      <div>
+        <strong>Long Axis:</strong>
+        <span>{{spec.requiredDimensions.longAxis}} inches</span>
+      </div>
+      <div>
+        <strong>Height:</strong>
+        <span>{{spec.requiredDimensions.height}} inches</span>
+      </div>
+    </div>
+    <div>
+      <div v-if="spec.filament">
+        <strong>Filament:</strong>
+        <span>{{spec.filament}}</span>
+      </div>
+      <div>
+        <strong>Support material:</strong>
+        <span>{{spec.requiresSupportMaterial ? 'Yes' : 'No'}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["spec"],
+  name: "PrinterSpec"
+};
+</script>
+
+<style>
+</style>
