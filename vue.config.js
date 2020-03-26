3decda7333bf9b63e7f05c90afe4afc859f11e98
Josiah Roberts <josiahroberts@pop-os.localdomain>
2020-03-26 15:14:30 +0800

    Get so it should work on bluehat

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    publicPath: '/lifemaker/',
-    runtimeCompiler: true,
-    chainWebpack: config => {
-        config.module.rule('markdown')
-            .test(/\.md$/)
-            .use('html-loader')
-            .loader('html-loader')
-            .end()
-            .use('markdown-loader')
-            .loader('markdown-loader')
-            .end();
-    }
-}
+  publicPath: "/",
+  runtimeCompiler: true,
+  chainWebpack: config => {
+    config.module
+      .rule("markdown")
+      .test(/\.md$/)
+      .use("html-loader")
+      .loader("html-loader")
+      .end()
+      .use("markdown-loader")
+      .loader("markdown-loader")
+      .end();
+  }
+};
