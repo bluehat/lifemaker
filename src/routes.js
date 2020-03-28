544685eb3624867ea5222e43735224eb7cf92506
bluehat <bluehat@fool.local>
2020-03-28 16:00:39 +0800

    minor markdown edits

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Manufacturing from "./pages/Manufacturing.vue";
 import Readme from "./pages/Readme.vue";
 import Features from "./pages/Features.vue"
 import Other_resources from "./pages/Other_resources.vue"
+import Safety from "./pages/Safety.vue"
 
 export default [
   {
@@ -36,5 +37,10 @@ export default [
     path: "/features",
     name: "features",
     component: Features
+  },
+  {
+    path: "/safety",
+    name: "safety",
+    component: Safety
   }
 ];
